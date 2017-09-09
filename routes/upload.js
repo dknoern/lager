08c51542749007d22a4824c3b0bf9dcb954a91eb
David Knoernschild <david@seattleweb.com>
2017-09-09 10:12:43 -0400

    initial upload capability

--- routes/upload.js
+++ routes/upload.js
@@ -1,21 +1,27 @@
 var express = require('express');
+var multer = require( 'multer' );
 var router = express.Router();
 
-//var flowjs = require('flow')
-
 router.use(function (req, res, next) {
-    next();
+  next();
 });
 
+
+var upload = multer({dest: 'uploads/'});
+
 router.route('/upload')
-    .post(function (req, res) {
-            return res.send("post...");
-    })
-    .put(function (req, res) {
-            return res.send("put...");
-    })
-    .get(function (req, res) {
-      return res.send("get...");
-    });
+.post(upload.single('file'),function (req, res) {
+
+  console.log("file uploaded");
+
+  console.log("upload request: %j", req.file);
+  return res.send("post...");
+})
+.put(function (req, res) {
+  return res.send("put...");
+})
+.get(function (req, res) {
+  return res.send("get...");
+});
 
 module.exports = router;
