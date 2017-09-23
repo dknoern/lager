d7480d2fb0e5e4e860e9d9512bc302eecced68bf
David Knoernschild <david@seattleweb.com>
2017-09-23 10:15:05 -0700

    remove lineItem cruft

--- routes/invoices.js
+++ routes/invoices.js
@@ -2,10 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 var Invoice = require('../models/invoice');
-var LineItem = require('../models/lineItem');
 
 var mongoose = require('mongoose');
-
 
 router.route('/invoices')
     .post(function(req, res) {
@@ -141,30 +139,5 @@ router.route('/invoices/:invoice_id')
             });
         });
     });
-
-router.route('/invoices/:invoice_id/items')
-    .post(function(req, res) {
-        Invoice.findById(req.params.invoice_id, function(err, invoice) {
-            if (err)
-                res.send(err);
-
-            var lineItem = new LineItem;
-            lineItem.name = req.body.name;
-            lineItem.amount = req.body.amount;
-            lineItem.serialNo = req.body.serialNo;
-            lineItem.itemNo = req.body.itemNo;
-
-            invoice.lineItems.push(lineItem);
-
-            invoice.save(function(err) {
-                if (err)
-                    res.send(err);
-
-                res.json({
-                    message: 'Invoice updated!'
-                });
-            });
-        });
-    })
 
 module.exports = router;
