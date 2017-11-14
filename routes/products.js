5be92db839f0f647f24e798350fb34bdbf3410b8
David Knoernschild <david@seattleweb.com>
2017-11-14 07:57:33 -0800

    add clone and at/back from show

--- routes/products.js
+++ routes/products.js
@@ -9,118 +9,130 @@ router.use(function(req, res, next) {
 });
 
 
-var getNextSequence = function(){
-  Counter.findByIdAndUpdate({_id: 'productId'}, {$inc: {seq: 1}}, function (error, counter) {
+var getNextSequence = function() {
+    Counter.findByIdAndUpdate({
+        _id: 'productId'
+    }, {
+        $inc: {
+            seq: 1
+        }
+    }, function(error, counter) {
         return counter.seq;
     });
 }
 
-var upsertProduct = function(req,res,productId, action)
-{
-  var paymentAmount = req.body.paymentAmount || 0;
-  var totalRepairCost = req.body.totalRepairCost || 0;
-  var cost = paymentAmount + totalRepairCost;
+var upsertProduct = function(req, res, productId, action) {
+    var paymentAmount = req.body.paymentAmount || 0;
+    var totalRepairCost = req.body.totalRepairCost || 0;
+    var cost = paymentAmount + totalRepairCost;
 
-  Product.findOneAndUpdate({_id: productId}, {
+    Product.findOneAndUpdate({
+        _id: productId
+    }, {
 
-      "$push": {
-          "history": {
-              user: req.user['http://mynamespace/name'],
-              date: Date.now(),
-              action: action
-          }
-      },
-      "$set": {
-          "_id" : productId,
-          "title": req.body.title,
-          "productType": req.body.productType,
-          "manufacturer": req.body.manufacturer,
-          "paymentAmount": paymentAmount,
-          "paymentMethod": req.body.paymentMethod,
-          "paymentDetails": req.body.paymentDetails,
-          "model": req.body.model,
-          "modelNumber": req.body.modelNumber,
-          "condition": req.body.condition,
-          "gender": req.body.gender,
-          "features": req.body.features,
-          "case": req.body.case,
-          "size": req.body.size,
-          "dial": req.body.dial,
-          "bracelet": req.body.bracelet,
-          "comments": req.body.comments,
-          "serialNo": req.body.serialNo,
-          "longDesc": req.body.longDesc,
-          "supplier": req.body.supplier,
-          "cost": cost,
-          "listPrice": req.body.listPrice || 0,
-          "totalRepairCost": totalRepairCost,
-          "notes": req.body.notes,
-          "ebayNoReserve": req.body.ebayNoReserve,
-          "inventoryItem": req.body.inventoryItem,
-          "seller": req.body.seller,
-          "sellerType": req.body.sellerType,
-          "lastUpdated" : Date.now(),
-          "status": req.body.status
-      }
-  }, {
-      upsert: true
-  }, function(err, doc) {
-      if (err) return res.send(500, {error: err  });
-      return res.send("succesfully saved");
-  });
+        "$push": {
+            "history": {
+                user: req.user['http://mynamespace/name'],
+                date: Date.now(),
+                action: action
+            }
+        },
+        "$set": {
+            "_id": productId,
+            "title": req.body.title,
+            "productType": req.body.productType,
+            "manufacturer": req.body.manufacturer,
+            "paymentAmount": paymentAmount,
+            "paymentMethod": req.body.paymentMethod,
+            "paymentDetails": req.body.paymentDetails,
+            "model": req.body.model,
+            "modelNumber": req.body.modelNumber,
+            "condition": req.body.condition,
+            "gender": req.body.gender,
+            "features": req.body.features,
+            "case": req.body.case,
+            "size": req.body.size,
+            "dial": req.body.dial,
+            "bracelet": req.body.bracelet,
+            "comments": req.body.comments,
+            "serialNo": req.body.serialNo,
+            "longDesc": req.body.longDesc,
+            "supplier": req.body.supplier,
+            "cost": cost,
+            "listPrice": req.body.listPrice || 0,
+            "totalRepairCost": totalRepairCost,
+            "notes": req.body.notes,
+            "ebayNoReserve": req.body.ebayNoReserve,
+            "inventoryItem": req.body.inventoryItem,
+            "seller": req.body.seller,
+            "sellerType": req.body.sellerType,
+            "lastUpdated": Date.now(),
+            "status": req.body.status
+        }
+    }, {
+        upsert: true
+    }, function(err, doc) {
+        if (err) return res.send(500, {
+            error: err
+        });
+        return res.send("succesfully saved");
+    });
 }
 
 router.route('/instock')
 
-.get(checkJwt, function(req, res) {
+    .get(checkJwt, function(req, res) {
 
-   var query = "";
-   var status = 'In Stock';
-   //var status = req.query.status;
+        var query = "";
+        var status = 'In Stock';
+        //var status = req.query.status;
 
-   if (status != null) {
+        if (status != null) {
 
-       Product.find({
-           'status': status
-       }, function(err, products) {
-           if (err) res.send(err);
-           res.json(products);
-       });
-       query = "status:" + status;
-   } else {
-       Product.find({}, function(err, products) {
-           if (err) res.send(err);
-           res.json(products);
-       });
-   }
-   console.log("looking for products with status=" + status);
-});
-
-
-
-
-
+            Product.find({
+                'status': status
+            }, function(err, products) {
+                if (err) res.send(err);
+                res.json(products);
+            });
+            query = "status:" + status;
+        } else {
+            Product.find({}, function(err, products) {
+                if (err) res.send(err);
+                res.json(products);
+            });
+        }
+        console.log("looking for products with status=" + status);
+    });
 
 router.route('/products')
     .post(checkJwt, function(req, res) {
 
         if (req.body._id == null) {
-          if(req.body.sellerType == 'Partner'){
-            req.body.status = 'Partnership';
-          }else{
-            req.body.status = 'In Stock';
-          }
-          Counter.findByIdAndUpdate({_id: 'productNumber'}, {$inc: {seq: 1}}, function (err, counter) {
-                if (err){
-                  console.log(err);
-                  return res.send(500, {error: err  });
+            if (req.body.sellerType == 'Partner') {
+                req.body.status = 'Partnership';
+            } else {
+                req.body.status = 'In Stock';
+            }
+            Counter.findByIdAndUpdate({
+                _id: 'productNumber'
+            }, {
+                $inc: {
+                    seq: 1
                 }
-                return upsertProduct(req,res,counter.seq,"product created");
-          });
-        }else{
-          return upsertProduct(req,res,req.body._id,"product updated");
+            }, function(err, counter) {
+                if (err) {
+                    console.log(err);
+                    return res.send(500, {
+                        error: err
+                    });
+                }
+                return upsertProduct(req, res, counter.seq, "product created");
+            });
+        } else {
+            return upsertProduct(req, res, req.body._id, "product updated");
         }
-      })
+    })
 
     //.get(checkJwt, function(req, res) {
     .get(function(req, res) {
@@ -157,21 +169,22 @@ router.route('/products')
 
             //Product.find({'title': new RegExp(search, 'i') }, function(err, products) {
             Product.find({
-              $and: [
-                {status: {$ne: "Deleted"}}
-                ,{
-                  $or: [{
-                    'title': new RegExp(search, 'i')
-                  },
-                  {
-                    'serialNo': new RegExp(search, 'i')
-                  },
-                  {
-                    'model': new RegExp(search, 'i')
-                  }
-                ]
-              }
-            ]
+                $and: [{
+                    status: {
+                        $ne: "Deleted"
+                    }
+                }, {
+                    $or: [{
+                            'title': new RegExp(search, 'i')
+                        },
+                        {
+                            'serialNo': new RegExp(search, 'i')
+                        },
+                        {
+                            'model': new RegExp(search, 'i')
+                        }
+                    ]
+                }]
             }, function(err, products) {
 
                 if (err)
@@ -182,28 +195,32 @@ router.route('/products')
                     var statusBadge = "";
 
                     var badgeStyle = "default"; // grey
-                    if (products[i].status == 'In Stock' || products[i].status == 'Partnership' ||  products[i].status == 'Problem')
-                      badgeStyle = "success"; // green
-                    else if (products[i].status =='Repair' || products[i].status =='Memo' || products[i].status =='At Show')
-                      badgeStyle = "warning" // yellow
-                      else if (products[i].status =='Sale Pending')
+                    if (products[i].status == 'In Stock' || products[i].status == 'Partnership' || products[i].status == 'Problem')
+                        badgeStyle = "success"; // green
+                    else if (products[i].status == 'Repair' || products[i].status == 'Memo' || products[i].status == 'At Show')
+                        badgeStyle = "warning" // yellow
+                    else if (products[i].status == 'Sale Pending')
                         badgeStyle = "danger" // red
 
                     results.data.push(
                         [
 
 
-                          '<a href=\"#\" onclick=\"selectProduct(\'' + products[i]._id + '\');return false;\">' + products[i]._id + '</a>',
+                            '<a href=\"#\" onclick=\"selectProduct(\'' + products[i]._id + '\');return false;\">' + products[i]._id + '</a>',
                             //'<a href=\"/#/app/item/' + products[i]._id + '\">' + products[i]._id,
                             products[i].title,
                             products[i].serialNo,
                             products[i].modelNumber,
-                             "<span class=\"badge bg-"+badgeStyle+"\">"+products[i].status+"</span>"
+                            "<span class=\"badge bg-" + badgeStyle + "\">" + products[i].status + "</span>"
                         ]
                     );
                 }
 
-                Product.count({status: {$ne: "Deleted"}}, function(err, count) {
+                Product.count({
+                    status: {
+                        $ne: "Deleted"
+                    }
+                }, function(err, count) {
                     results.recordsTotal = count;
 
                     if (search == '' || search == null) {
@@ -212,21 +229,22 @@ router.route('/products')
                     } else {
                         Product.count({
 
-                          $and: [
-                            {status: {$ne: "Deleted"}}
-                            ,{
-                              $or: [{
-                                'title': new RegExp(search, 'i')
-                              },
-                              {
-                                'serialNo': new RegExp(search, 'i')
-                              },
-                              {
-                                'model': new RegExp(search, 'i')
-                              }
-                            ]
-                          }
-                        ]
+                            $and: [{
+                                status: {
+                                    $ne: "Deleted"
+                                }
+                            }, {
+                                $or: [{
+                                        'title': new RegExp(search, 'i')
+                                    },
+                                    {
+                                        'serialNo': new RegExp(search, 'i')
+                                    },
+                                    {
+                                        'model': new RegExp(search, 'i')
+                                    }
+                                ]
+                            }]
 
                         }, function(err, count) {
                             results.recordsFiltered = count;
@@ -283,31 +301,92 @@ router.route('/products/:product_id')
 
     .delete(checkJwt, function(req, res) {
 
-      Product.findById(req.params.product_id, function(err, product) {
-          if (err)
-              res.send(err);
-          product.status = 'Deleted';
-          product.save(function(err) {
+        Product.findById(req.params.product_id, function(err, product) {
+            if (err)
+                res.send(err);
+            product.status = 'Deleted';
+            product.save(function(err) {
+                if (err)
+                    res.send(err);
+                res.json({
+                    message: 'Product updated!'
+                });
+            });
+        });
+
+        /*
+          Product.remove({
+              _id: req.params.product_id
+          }, function(err, product) {
               if (err)
                   res.send(err);
               res.json({
-                  message: 'Product updated!'
+                  message: 'Successfully deleted'
               });
           });
-      });
-
-      /*
-        Product.remove({
-            _id: req.params.product_id
-        }, function(err, product) {
-            if (err)
-                res.send(err);
-            res.json({
-                message: 'Successfully deleted'
-            });
-        });
-        */
+          */
 
     });
+
+
+
+
+
+
+
+
+
+router.route('/products/:product_id/status')
+    .put(checkJwt, function(req, res) {
+
+        var newStatus = req.body.status;
+
+        console.log('setting status to ' + newStatus);
+
+        var responseData = {
+            "status": newStatus
+        }
+
+        Product.findOneAndUpdate({
+            _id: req.params.product_id
+        }, {
+
+            "$push": {
+                "history": {
+                    user: req.user['http://mynamespace/name'],
+                    date: Date.now(),
+                    action: "item " + newStatus.toLowerCase()
+                }
+            },
+            "$set": {
+                "lastUpdated": Date.now(),
+                "status": req.body.status
+            }
+        }, {
+            upsert: true
+        }, function(err, doc) {
+            if (err)
+                res.send(err);
+            res.json(responseData);
+        });
+
+
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = router;
