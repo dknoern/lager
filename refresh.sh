cc3f64b89077281e6be68ba0b8dd2cefc5d6e525
David Knoernschild <david@seattleweb.com>
2018-03-29 09:00:49 -0700

    add misc item to invoice

--- refresh.sh
+++ refresh.sh
@@ -1,7 +1,8 @@
 node refresh.js products
 node refresh.js customers
 node refresh.js invoices
-node refresh.js invoicedetails
+node refresh.js invoicedetails1
+node refresh.js invoicedetails2
 node refresh.js returns
 node refresh.js returndetails
 node refresh.js repairs
