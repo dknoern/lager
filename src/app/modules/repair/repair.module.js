f6fe37ef3b29e36211d1049c66e401276c48a869
David Knoernschild <david@seattleweb.com>
2018-06-14 10:30:04 -0700

    new repair invoice screen

--- src/app/modules/repair/repair.module.js
+++ src/app/modules/repair/repair.module.js
@@ -1,32 +1,41 @@
+(function () {
+    'use strict';
+
+    var module = angular.module('singApp.repair', [
+        'ui.router',
+        'ui.jq',
+        'ui.event',
+        'ngResource',
+        'singApp.components.dropzone',
+        'singApp.components.switchery',
+        'singApp.components.holderjs',
+        'angular-bootstrap-select',
+        'summernote'
+    ]);
+
+    module.config(appConfig);
+
+    appConfig.$inject = ['$stateProvider'];
+
+    function appConfig($stateProvider) {
+        $stateProvider
+            .state('app.repair', {
+                url: '/repair/:repairId',
+                templateUrl: 'app/modules/repair/repair.html',
+                controller: function ($scope, $stateParams) {
+                    $scope.repairId = $stateParams.repairId;
+                }
+            })
+
+        $stateProvider
+            .state('app.repairedit', {
+                url: '/repair/edit/:repairId',
+                templateUrl: 'app/modules/repair/edit-repair.html',
+                controller: function ($scope, $stateParams) {
+                    $scope.repairId = $stateParams.repairId;
+                }
+            })
 
 
-(function() {
-  'use strict';
-
-  var module = angular.module('singApp.repair', [
-    'ui.router',
-    'ui.jq',
-    'ui.event',
-    'ngResource',
-    'singApp.components.dropzone',
-    'singApp.components.switchery',
-    'singApp.components.holderjs',
-    'angular-bootstrap-select',
-    'summernote'
-  ]);
-
-  module.config(appConfig);
-
-  appConfig.$inject = ['$stateProvider'];
-
-  function appConfig($stateProvider) {
-    $stateProvider
-      .state('app.repair', {
-        url: '/repair/:repairId',
-        templateUrl: 'app/modules/repair/repair.html',
-        controller: function($scope, $stateParams) {
-          $scope.repairId = $stateParams.repairId;
-        }
-      })
-  }
+    }
 })();
