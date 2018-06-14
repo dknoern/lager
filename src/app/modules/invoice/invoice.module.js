3019c73a4ca7d6ebeb4aed1be0916f01008f72de
David Knoernschild <david@seattleweb.com>
2018-06-14 10:30:04 -0700

    new repair invoice screen

--- src/app/modules/invoice/invoice.module.js
+++ src/app/modules/invoice/invoice.module.js
@@ -1,55 +1,49 @@
+(function () {
+    'use strict';
 
+    var module = angular.module('singApp.invoice', [
+        'ui.router',
+        'ui.jq',
+        'ui.event',
+        'ngResource',
+        'singApp.components.dropzone',
+        'singApp.components.switchery',
+        'singApp.components.holderjs',
+        'angular-bootstrap-select',
+        'summernote'
+    ]);
 
-(function() {
-  'use strict';
+    module.config(appConfig);
 
-  var module = angular.module('singApp.invoice', [
-    'ui.router',
-    'ui.jq',
-    'ui.event',
-    'ngResource',
-    'singApp.components.dropzone',
-    'singApp.components.switchery',
-    'singApp.components.holderjs',
-    'angular-bootstrap-select',
-    'summernote'
-  ]);
+    appConfig.$inject = ['$stateProvider'];
 
-  module.config(appConfig);
-
-  appConfig.$inject = ['$stateProvider'];
-
-  function appConfig($stateProvider) {
-    $stateProvider
-      .state('app.invoice', {
-        url: '/invoice/edit/:invoiceId',
-        templateUrl: 'app/modules/invoice/invoice.html',
-        controller: function($scope, $stateParams) {
-          $scope.invoiceId = $stateParams.invoiceId;
-
-        }
-      });
-
-
+    function appConfig($stateProvider) {
         $stateProvider
-          .state('app.extrainvoice', {
-            url: '/invoice/:invoiceId',
-            templateUrl: 'app/modules/invoice/extra-invoice.html',
-            controller: function($scope, $stateParams) {
-              $scope.invoiceId = $stateParams.invoiceId;
-            }
-          });
+            .state('app.invoice', {
+                url: '/invoice/edit/:invoiceId',
+                templateUrl: 'app/modules/invoice/invoice.html',
+                controller: function ($scope, $stateParams) {
+                    $scope.invoiceId = $stateParams.invoiceId;
 
-          $stateProvider
-            .state('app.partnerinvoice', {
-              url: '/partnerinvoice/:productId',
-              templateUrl: 'app/modules/invoice/extra-invoice.html',
-              controller: function($scope, $stateParams) {
-                $scope.productId = $stateParams.productId;
-              }
+                }
             });
 
+        $stateProvider
+            .state('app.extrainvoice', {
+                url: '/invoice/:invoiceId',
+                templateUrl: 'app/modules/invoice/extra-invoice.html',
+                controller: function ($scope, $stateParams) {
+                    $scope.invoiceId = $stateParams.invoiceId;
+                }
+            });
 
-
-  }
+        $stateProvider
+            .state('app.partnerinvoice', {
+                url: '/partnerinvoice/:productId',
+                templateUrl: 'app/modules/invoice/extra-invoice.html',
+                controller: function ($scope, $stateParams) {
+                    $scope.productId = $stateParams.productId;
+                }
+            });
+    }
 })();
