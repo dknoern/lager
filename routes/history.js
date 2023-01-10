fa1bbad848ee22783279aca0d11be41777c1bcb5
David Knoernschild <david@seattleweb.com>
2023-01-10 11:55:01 -0800

    Fix repair history failure

--- routes/history.js
+++ routes/history.js
@@ -2,10 +2,14 @@ var Product = require('../models/product');
 
 module.exports.updateProductHistory = async function (lineItems, status, action, user, refDoc) {
 
+    console.log("updatingProductHistory for ",JSON.stringify(lineItems),status,action,user,refDoc);
+
+
+
     for (var i = 0; lineItems != null && i < lineItems.length; i++) {
         var lineItem = lineItems[i];
 
-        if (lineItem.productId != null && lineItem.itemNumber != null) {
+        if (lineItem.productId != null) {
 
             var historyEntry = {
                 user: user,
@@ -17,7 +21,7 @@ module.exports.updateProductHistory = async function (lineItems, status, action,
                 historyEntry.refDoc = refDoc;
             }
 
-            console.log("updating product history for ",lineItem.itemNumber,"status",status);
+            console.log("updating product history for productId",lineItem.productId,"status",status);
 
             await Product.findOneAndUpdate({
                 _id: lineItem.productId,
