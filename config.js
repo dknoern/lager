16681af879722c9092191c3e0a2498fa74215cf3
David Knoernschild <david@seattleweb.com>
2025-10-12 12:09:38 -0700

    update mongo URI

--- config.js
+++ config.js
@@ -64,7 +64,7 @@ const auth0 = {
 
 // MongoDB URL
 const mongo = {
-    url: process.env.MONGO_URL || 'mongodb://localhost:27017/lager'
+    url: process.env.MONGODB_URI || 'mongodb://localhost:27017/lager'
 };
 
 module.exports = {
